bff0f86f62be8a7543cf22ea3e0a91934d36c9c5
Habiba Musa <habibamusazee@gmail.com>
2025-11-05 13:19:44 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+
+# Load your trained model
+model = YOLO("/content/drive/MyDrive/AI Capstone Project/faw_model_trained.pt")
+
+st.title("🌽 Fall Armyworm Early Detection System")
+st.write("Upload a maize leaf image to detect Fall Armyworm or related conditions.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
+
+    if st.button("Detect"):
+        results = model.predict(image, conf=0.25)
+        st.image(results[0].plot(), caption="Detection Result", use_container_width=True)
